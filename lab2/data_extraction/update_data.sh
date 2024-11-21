e122beeb8335370093e07e11284ad4cdc8479edf
Esteban Bernalc <stipgod2@gmail.com>
2024-11-21 16:08:25 -0500

    Change origin url

--- lab2/data_extraction/update_data.sh
+++ lab2/data_extraction/update_data.sh
@@ -2,7 +2,7 @@ git config --global user.name 'Esteban Bernalc'
 git config --global user.email 'stipgod2@gmail.com'
 # DEBE CONFIGURAR UNA VARIABLE DE AMBIENTE GH_TOKEN EN Github-Setttings-> Develope Settings -> Personal access token - > Tokens
 # LUEGO DENTRO DEL REPOSITORIO: Settings -> Secrets and variables -> Actions -> Secrets -> Environment secrets
-git remote set-url origin https://x-access-token:${{ secrets.PAT }}@github.com/edwinm67/mlops-ejemplo2.git
+git remote set-url origin https://x-access-token:${{ secrets.PAT }}@github.com/StipGod/mlops-ebernalc-jjsanchezc
 git add .
 set +e 
 git status | grep modified
